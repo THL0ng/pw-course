9815bd52e5259c07cdf32baf5691ec6723be0869
PC <thanhlongzz412zz@gmail.com>
2026-05-10 21:43:06 +0700

    lesson-03

--- tests/lesson-02/ex6.js
+++ tests/lesson-02/ex6.js
@@ -1,5 +1,3 @@
-for(let i = 0; i <= 100; i++) {
-    if(i >=12 && i<=30) {
-        console.log("Số " +  i + " là số nằm trong khoảng 12-30");
+for(let i = 12; i <= 30; i++) {
+        console.log(i);
     }
-}
